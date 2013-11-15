5fe3d3a973880df4a13908260a55e665c70f8fec
fedeagb@gmail.com <fedeagb@gmail.com@f4e307e5-2586-7bd0-c4b4-977e71a634f3>
2013-11-15 23:32:31 +0000

    Funcion procesarArchivos

---
+++ procesar.c
@@ -0,0 +1,8 @@
+#include <procesar.h>
+
+// Procesa el archivo de entrada (puede ser stdin)
+// Invierte las lineas del archivo y las imprime por stdout
+// En caso satisfactorio devuelve 0, distinto de 0 en otros casos
+int procesarArchivo(int fd) {
+
+}
